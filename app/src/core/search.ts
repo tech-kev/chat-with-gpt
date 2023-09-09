16a2c10eac61d63721d0bdfe7545dbce52b4c8cd
tech-kev <github@kevin-theil.de>
2023-09-09 21:53:32 +0200

    fixed wrong recentChat url

--- app/src/core/search.ts
+++ app/src/core/search.ts
@@ -41,7 +41,7 @@ export class Search {
         if (!query?.trim().length) {
             const searchResults = this.context.all()
                 .sort((a, b) => b.updated - a.updated)
-                .slice(0, 10);
+                .slice(0, 99999);
             const results = this.processSearchResults(searchResults);
             return results;
         }
